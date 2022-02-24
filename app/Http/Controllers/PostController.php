7da83b54caf22c71c72446efc85c7a25a105e868
Fstudent <98zinitu0730@gmail.com>
2022-02-24 17:47:32 +0900

    バリデーション、エラー文表示

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Post;
+use App\Http\Requests\PostRequest;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -19,11 +20,10 @@ class PostController extends Controller
     {
         return view('posts/create');
     }
-    public function store(Request $request, Post $post)
+    public function store(Post $post, PostRequest $request)
     {
         $input = $request['post'];
         $post->fill($input)->save();
         return redirect('/posts/' . $post->id);
     }
 }
-?>
